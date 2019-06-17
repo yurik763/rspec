c4ad9e1ddb1a5cf01c0699d37e579bd3d458227a
yurik763 <yurik763@tut.by>
2019-06-17 22:40:08 +0300

    Add hero entity

--- hero.rb
+++ hero.rb
@@ -1,2 +1,29 @@
 class Hero
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def name
+    @name # для вывода имени нашего героя
+  end
+
+  def power_up
+    @health +=10
+  end
+
+  def power_down
+    @health -=10
+  end
+
+  def hero_info
+    "#{@name} has a health of #{@health}"
+  end
+
 end
+
+hero = Hero.new 'mike'
+puts hero.name
+puts hero.power_up
+puts hero.power_down
+puts hero.hero_info
