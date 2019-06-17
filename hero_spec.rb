a7cffe61458800d1fba3d9739dac6a5ca8a8faa3
yurik763 <yurik763@tut.by>
2019-06-17 23:11:52 +0300

    Add test for power_up

--- hero_spec.rb
+++ hero_spec.rb
@@ -8,4 +8,10 @@ describe Hero do
     expect(hero.name).to eq 'Mike' #аналог hero.name = 'Mike'
   end
 
+  it "can power up" do
+    hero = Hero.new 'mike'
+
+    expect(hero.power_up).to eq 110 #должно быть равно 110
+  end
+
 end
